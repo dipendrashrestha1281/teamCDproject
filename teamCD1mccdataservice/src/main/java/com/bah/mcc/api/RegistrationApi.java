7d587a63d80181b1ace0f44c946d48eb5d778640
user <user@this.com>
2022-08-15 13:15:07 -0400

    Added Controllers from previous project

---
+++ teamCD1mccdataservice/src/main/java/com/bah/mcc/api/RegistrationApi.java
@@ -0,0 +1,51 @@
+package com.bah.mcc.api;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.bah.msd.mcc.domain.Registration;
+
+@RestController
+@RequestMapping("/registrations")
+public class RegistrationApi {
+
+	// Dummy Data
+	ArrayList<Registration> registrationList = new ArrayList<Registration>();
+
+	public RegistrationApi() {
+		Registration r1 = new Registration(1L, "1", "2", new Date(), "please email me the event details");
+		Registration r2 = new Registration(2L, "2", "2", new Date(), "send transportation and hotel booking");
+		Registration r3 = new Registration(3L, "3", "3", new Date(), "defer payments for a week");
+		
+		registrationList.add(r1);
+		registrationList.add(r2);
+		registrationList.add(r3);
+	}
+
+	// Get All
+	@GetMapping
+	public Collection<Registration> getAll() {
+		return this.registrationList;
+	}
+
+	// Get One
+	@GetMapping("/{registrationId}")
+	public Registration getOneById(@PathVariable("registrationId") long id) {
+
+		// Set to null
+		Registration registration = null;
+		
+		// return if ID matches
+		for (int i = 0; i < registrationList.size(); i++) {
+			if (registrationList.get(i).getId() == id) {
+				registration = registrationList.get(i);
+			}
+		}
+		// If no match return registration as null
+		return registration;
+	}
+
+}
--- teamCD1mccdataservice/src/main/java/com/bah/mcc/api/Test.java
+++
@@ -1,5 +0,0 @@
-package com.bah.mcc.api;
-
-public class Test {
-
-}
