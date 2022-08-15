9d76c48118c5ee2c14715a5bb06fad8a44ca3fd3
user <user@this.com>
2022-08-15 11:27:25 -0400

    First commit

---
+++ teamCD1mccdataservice/src/main/java/com/bah/msd/mcc/TeamCd1mccdataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCd1mccdataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCd1mccdataserviceApplication.class, args);
+	}
+
+}
