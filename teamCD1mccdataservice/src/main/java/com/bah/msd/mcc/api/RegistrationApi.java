7894c7b0889de73bf449c6ea983b2a26b8f95336
user <user@this.com>
2022-08-15 16:17:37 -0400

    Finish

--- teamCD1mccdataservice/src/main/java/com/bah/msd/mcc/api/RegistrationApi.java
+++ teamCD1mccdataservice/src/main/java/com/bah/msd/mcc/api/RegistrationApi.java
@@ -1,12 +1,16 @@
 package com.bah.msd.mcc.api;
+
 import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,72 +23,48 @@ import com.bah.msd.mcc.repository.RegistrationRepository;
 @RestController
 @RequestMapping("/registrations")
 public class RegistrationApi {
-	
+
 	@Autowired
 	RegistrationRepository repo;
-	
+
+	@GetMapping
 	public Iterable<Registration> getAll() {
 		return repo.findAll();
 	}
-	
-	@GetMapping("/{registrationId")
+
+	@GetMapping("/{registrationId}")
 	public Optional<Registration> getRegistrationById(@PathVariable("registrationId") long id) {
 		return repo.findById(id);
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<?> addRegistration(@RequestBody Registration newRegistration, UriComponentsBuilder uri) {
-		if (newRegistration.getId() != 0 
-			|| newRegistration.getEvent_id() == null
-			|| newRegistration.getCustomer_id() == null
-			|| newRegistration.getRegistration_date() == null) {
-			
+		if (newRegistration.getId() != 0 || newRegistration.getEvent_id() == null
+				|| newRegistration.getCustomer_id() == null || newRegistration.getRegistration_date() == null) {
+
 			return ResponseEntity.badRequest().build();
-			
+
 		}
 		newRegistration = repo.save(newRegistration);
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(newRegistration.getId()).toUri();
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(newRegistration.getId()).toUri();
 		ResponseEntity<?> response = ResponseEntity.created(location).build();
 		return response;
 	}
-	
-	
 
-	// Dummy Data
-//	ArrayList<Registration> registrationList = new ArrayList<Registration>();
-//
-//	public RegistrationApi() {
-//		Registration r1 = new Registration(1L, "1", "2", new Date(), "please email me the event details");
-//		Registration r2 = new Registration(2L, "2", "2", new Date(), "send transportation and hotel booking");
-//		Registration r3 = new Registration(3L, "3", "3", new Date(), "defer payments for a week");
-//		
-//		registrationList.add(r1);
-//		registrationList.add(r2);
-//		registrationList.add(r3);
-//	}
-//
-//	// Get All
-//	@GetMapping
-//	public Collection<Registration> getAll() {
-//		return this.registrationList;
-//	}
-//
-//	// Get One
-//	@GetMapping("/{registrationId}")
-//	public Registration getOneById(@PathVariable("registrationId") long id) {
-//
-//		// Set to null
-//		Registration registration = null;
-//		
-//		// return if ID matches
-//		for (int i = 0; i < registrationList.size(); i++) {
-//			if (registrationList.get(i).getId() == id) {
-//				registration = registrationList.get(i);
-//			}
-//		}
-//		// If no match return registration as null
-//		return registration;
-//	}
+	@PutMapping("/{registrationId}")
+	public ResponseEntity<?> putRegistration(@RequestBody Registration newRegistration, @PathVariable("registrationId") long eventId) {
+		if (newRegistration.getEvent_id() == null || newRegistration.getCustomer_id() == null) { 
+			return ResponseEntity.badRequest().build();
+		}
+		newRegistration = repo.save(newRegistration);
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping("/{registrationId}")
+	public ResponseEntity<?> deleteRegistrationById(@PathVariable("registrationId") long id) {
+		repo.deleteById(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 
 }
